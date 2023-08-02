361e248f22ee351c48b069aee7c60fe06a5d2afe
Yunpei24 <nikiemajoshua24@gmail.com>
2023-08-02 16:13:31 +0000

    mise à jour routes

--- routes/country_api.py
+++ routes/country_api.py
@@ -264,6 +264,6 @@ async def get_distinct_request(query: str):
     Returns:
     JSONResponse: Un objet JSONResponse contenant les données des pays au format JSON.
     """
-    query = json.loads(query)
+    #query = json.loads(query)
     df = getDistinctRequest(query, mycollection)
     return JSONResponse(content=df.to_dict(orient="records"))
